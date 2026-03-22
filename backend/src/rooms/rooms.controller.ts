3620fb4007d2ac76ab9961bab3afc81ff8e74e5f
kyuubi722 <sebastiansasu499@gmail.com>
2026-03-22 14:51:06 +0200

    adadasad

--- backend/src/rooms/rooms.controller.ts
+++ backend/src/rooms/rooms.controller.ts
@@ -2,6 +2,7 @@ import {
   Body,
   Controller,
   Delete,
+  Query,
   Get,
   Param,
   Patch,
@@ -14,6 +15,38 @@ import { RoomsService } from './rooms.service';
 @Controller('rooms')
 export class RoomsController {
   constructor(private readonly roomsService: RoomsService) {}
+
+  // --- ACȚIUNI SPECIFICE (Trebuie să fie SUS pentru a evita conflicte) ---
+
+  // RUTA: DELETE /rooms/action/end/:id?nickname=...
+  @Delete('action/end/:id')
+  async endRoom(
+    @Param('id') id: string, 
+    @Query('nickname') nickname: string
+  ) {
+    console.log(`Request to end room ${id} by ${nickname}`);
+    return this.roomsService.endRoom(id, nickname);
+  }
+
+  // RUTA: DELETE /rooms/action/leave/:id/:userId
+  @Delete('action/leave/:id/:userId')
+  async leave(
+    @Param('id') id: string, 
+    @Param('userId') userId: string
+  ) {
+    return this.roomsService.leaveRoom(id, userId);
+  }
+
+  // RUTA: POST /rooms/action/join/:id
+  @Post('action/join/:id')
+  async join(
+    @Param('id') id: string, 
+    @Body() participantDto: any
+  ) {
+    return this.roomsService.joinRoom(id, participantDto);
+  }
+
+  // --- CRUD STANDARD ---
 
   @Post()
   create(@Body() createRoomDto: CreateRoomDto) {
@@ -33,10 +66,5 @@ export class RoomsController {
   @Patch(':id')
   update(@Param('id') id: string, @Body() updateRoomDto: UpdateRoomDto) {
     return this.roomsService.update(id, updateRoomDto);
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.roomsService.remove(id);
   }
 }
